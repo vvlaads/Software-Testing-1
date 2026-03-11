0ba93d4d0e7b753f022527262eb3941f9a91a279
vvlaads <vvlaads@gmail.com>
2026-03-11 23:23:29 +0300

    Refactored DijkstraTest

--- src/test/java/com/swt/lab/task2/DijkstraTest.java
+++ src/test/java/com/swt/lab/task2/DijkstraTest.java
@@ -25,55 +25,55 @@ public class DijkstraTest {
         return matrix;
     }
 
+    private void addEdge(double[][] matrix, double weight, int i, int j) {
+        matrix[i][j] = weight;
+        matrix[j][i] = weight;
+    }
+
     @Test
     void testCommonInput() {
-        double[][] matrix = createMatrix(8);
-        matrix[0][1] = 7;
-        matrix[1][0] = 7;
-        matrix[1][3] = 6;
-        matrix[3][1] = 6;
-        matrix[3][5] = 5;
-        matrix[5][3] = 5;
-        matrix[6][4] = 8;
-        matrix[4][6] = 8;
-        matrix[5][7] = 9;
-        matrix[7][5] = 9;
-        List<Double> expected = List.of(13.0, 6.0, INF, 0.0, INF, 5.0, INF, 14.0);
-        assertEquals(expected, dijkstra.calculate(3, matrix));
+        double[][] matrix = createMatrix(3);
+        addEdge(matrix, 3, 0, 1);
+        List<Double> expected = List.of(0.0, 3.0, INF);
+        assertEquals(expected, dijkstra.calculate(0, matrix));
     }
 
     @Test
     void testIsolatedVertex() {
-        double[][] matrix = createMatrix(8);
-        matrix[0][1] = 7;
-        matrix[1][0] = 7;
-        matrix[1][3] = 6;
-        matrix[3][1] = 6;
-        matrix[3][5] = 5;
-        matrix[5][3] = 5;
-        matrix[6][4] = 8;
-        matrix[4][6] = 8;
-        matrix[5][7] = 9;
-        matrix[7][5] = 9;
-        List<Double> expected = List.of(INF, INF, 0.0, INF, INF, INF, INF, INF);
+        double[][] matrix = createMatrix(3);
+        addEdge(matrix, 3, 0, 1);
+        List<Double> expected = List.of(INF, INF, 0.0);
         assertEquals(expected, dijkstra.calculate(2, matrix));
     }
 
     @Test
     void testCycle() {
-        double[][] matrix = createMatrix(8);
-        matrix[1][3] = 8;
-        matrix[3][1] = 8;
-        matrix[1][5] = 1;
-        matrix[5][1] = 1;
-        matrix[3][5] = 9;
-        matrix[5][3] = 9;
-        matrix[3][7] = 1;
-        matrix[7][3] = 1;
-        matrix[4][7] = 2;
-        matrix[7][4] = 2;
-        List<Double> expected = List.of(INF, 1.0, INF, 9.0, 12.0, 0.0, INF, 10.0);
-        assertEquals(expected, dijkstra.calculate(5, matrix));
+        double[][] matrix = createMatrix(3);
+        addEdge(matrix, 5, 0, 1);
+        addEdge(matrix, 4, 1, 2);
+        addEdge(matrix, 2, 0, 2);
+        List<Double> expected = List.of(5.0, 0.0, 4.0);
+        assertEquals(expected, dijkstra.calculate(1, matrix));
+    }
+
+    @Test
+    void testGraphWithLoop() {
+        double[][] matrix = createMatrix(1);
+        addEdge(matrix, 5, 0, 0);
+        List<Double> expected = List.of(0.0);
+        assertEquals(expected, dijkstra.calculate(0, matrix));
+    }
+
+    @Test
+    void testGraphWithLoops() {
+        double[][] matrix = createMatrix(4);
+        addEdge(matrix, 5, 0, 1);
+        addEdge(matrix, 5, 1, 2);
+        addEdge(matrix, 5, 2, 3);
+        addEdge(matrix, 3, 1, 1);
+        addEdge(matrix, 3, 2, 2);
+        List<Double> expected = List.of(0.0, 5.0, 10.0, 15.0);
+        assertEquals(expected, dijkstra.calculate(0, matrix));
     }
 
     @Test
@@ -93,8 +93,7 @@ public class DijkstraTest {
     @Test
     void testNegativeEdge() {
         double[][] matrix = createMatrix(2);
-        matrix[0][1] = -3;
-        matrix[1][0] = -3;
+        addEdge(matrix, -3, 0, 1);
 
         assertThrows(IllegalArgumentException.class,
                 () -> dijkstra.calculate(0, matrix));
@@ -103,10 +102,8 @@ public class DijkstraTest {
     @Test
     void testBadMatrix() {
         double[][] badMatrix = new double[5][4];
-        badMatrix[0][1] = 4;
-        badMatrix[1][0] = 4;
-        badMatrix[1][3] = 6;
-        badMatrix[3][1] = 6;
+        addEdge(badMatrix, 4, 0, 1);
+        addEdge(badMatrix, 6, 1, 3);
 
         assertThrows(IllegalArgumentException.class, () -> dijkstra.calculate(3, badMatrix));
     }
